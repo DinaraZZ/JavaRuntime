b4117341ab1a3879de765f6e26be55c38b14f669
DinaraZZ <dinara.rakhymbekova@gmail.com>
2023-11-23 16:57:45 +0600

    20231123-cl

--- src/oop_prac14_compar/Schedule.java
+++ src/oop_prac14_compar/Schedule.java
@@ -2,7 +2,6 @@ package oop_prac14_compar;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,12 +14,14 @@ public class Schedule {
 
     public Schedule(Event... events) {
         this.events = new ArrayList<>();
-        for (Event e : events) addEvent(e);
-        sortSchedule();
+        for (Event e : events) addEventAnother(e);
+//        sortSchedule();
     }
 
     public void addEvent(Event event) {
+        // event to add start
         LocalTime start = event.getStartTime();
+        // event to add end
         LocalTime end = start.plusMinutes(event.getDuration());
 
         boolean accessible = true;
@@ -29,8 +30,17 @@ public class Schedule {
             LocalTime startCurrent = e.getStartTime();
             LocalTime endCurrent = startCurrent.plusMinutes(e.getDuration());
 
-            if ((start.compareTo(startCurrent) > 0 && start.compareTo(endCurrent) < 0) ||
-                    (end.compareTo(startCurrent) > 0 && end.compareTo(endCurrent) < 0)) {
+            // event to add -> 12:00 - 14:00
+            // current event -> 11:00 - 12:10
+            // 12:00 > 11:00 && 12:00 < 12:10 -> true
+
+            // event to add -> 12:00 - 14:00
+            // current event -> 13:50 - 14:30
+            // 12:00 > 13:50 && 12:00 < 14:30 -> false
+            // 14:00 > 13:50 && 14:00 < 14:30 -> true
+
+            if ((start.compareTo(startCurrent) >= 0 && start.compareTo(endCurrent) < 0) ||
+                    (end.compareTo(startCurrent) > 0 && end.compareTo(endCurrent) <= 0)) {
                 accessible = false;
                 break;
             }
@@ -41,6 +51,45 @@ public class Schedule {
         if (accessible) {
             events.add(event);
             sortSchedule();
+        }
+    }
+
+    public void addEventAnother(Event event) {
+        // event to add start
+        LocalTime start = event.getStartTime();
+        // event to add end
+        LocalTime end = start.plusMinutes(event.getDuration());
+
+        if (events.size() == 0) events.add(event);
+        else if (events.size() == 1) {
+            if (end.compareTo(events.get(0).getStartTime()) <= 0) events.add(0, event);
+            else if (start.compareTo(events.get(0).getStartTime().plusMinutes(events.get(0).getDuration())) >= 0)
+                events.add(event);
+        } else {
+            for (int i = 0; i < events.size(); i++) {
+                LocalTime startCurrent = events.get(i).getStartTime();
+                LocalTime endCurrent = startCurrent.plusMinutes(events.get(i).getDuration());
+                if (i == 0) {
+                    if (end.compareTo(startCurrent) <= 0) {
+                        events.add(0, event);
+                        break;
+                    }
+                } else {
+                    LocalTime startPrevios = events.get(i - 1).getStartTime();
+                    LocalTime endPrevios = startPrevios.plusMinutes(events.get(i - 1).getDuration());
+
+                    if (i == events.size() - 1) {
+                        if (start.compareTo(endCurrent) >= 0) {
+                            events.add(event);
+                            break;
+                        }
+                    }
+                    if (end.compareTo(startCurrent) <= 0 && start.compareTo(endPrevios) >= 0) {
+                        events.add(i, event);
+                        break;
+                    }
+                }
+            }
         }
     }
 
--- src/oop_prac14_compar/ScheduleMain.java
+++ src/oop_prac14_compar/ScheduleMain.java
@@ -6,11 +6,13 @@ import java.util.Arrays;
 
 public class ScheduleMain {
     public static void main(String[] args) {
-        Event e1 = new Event("E1", LocalTime.of(12, 00), 60);
-        Event e2 = new Event("E2", LocalTime.of(13, 00), 60);
+        Event e1 = new Event("E1", LocalTime.of(12, 0), 60);
+        Event e2 = new Event("E2", LocalTime.of(13, 0), 60);
         Event e3 = new Event("E3", LocalTime.of(14, 30), 60);
         Event e4 = new Event("E4", LocalTime.of(10, 30), 50);
-        Event[] events = new Event[]{e4, e2, e3, e1};
+        Event e5 = new Event("E5", LocalTime.of(9, 0), 10);
+        Event e6 = new Event("E6", LocalTime.of(9, 5), 10);
+        Event[] events = new Event[]{e4, e2, e3, e1, e5, e6};
 
         Schedule sc = new Schedule(events);
 
