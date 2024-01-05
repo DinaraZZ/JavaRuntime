2b0a5aa76496539f0a044871a809695d75dbdb63
DinaraZZ <dinara.rakhymbekova@gmail.com>
2024-01-05 13:08:14 +0600

    20240105

---
+++ src/oop_prac17_input_stream/User.java
@@ -0,0 +1,8 @@
+package oop_prac17_input_stream;
+
+public class User {
+    String login;
+    String password;
+    String name;
+    String income;
+}
---
+++ src/oop_prac17_input_stream/UsersMain2.java
@@ -0,0 +1,207 @@
+package oop_prac17_input_stream;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static oop_prac17_input_stream.Main.readLine;
+
+public class UsersMain2 {
+    public static void main(String[] args) {
+        findUserIO();
+    }
+
+    private static Map<String, String> users = new HashMap<>();
+    private static List<User> userList = new ArrayList<>();
+
+    public static void findUserIO() {
+        try (InputStreamReader streamReader = new InputStreamReader(System.in)) {
+            System.out.print("Введите логин: ");
+            String login = readLine(streamReader);
+            System.out.print("Введите пароль: ");
+            String password = readLine(streamReader);
+            System.out.println();
+            getUsersFromFile();
+            if (userExists(login, password)) optionsIO(login, password);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static boolean userExists(String login, String password) {
+        try {
+            if (users.get(login).equals(password)) return true;
+        } catch (NullPointerException e) {
+            return false;
+        }
+        return false;
+    }
+
+    public static void optionsIO(String login, String password) {
+        try (InputStreamReader streamReader = new InputStreamReader(System.in)) {
+            String line;
+            int action;
+            do {
+                System.out.println("Просмотреть данные [1]");
+                System.out.println("Изменить данные [2]");
+                System.out.println("Удалить данные [3]");
+                System.out.print("Выберите действие: ");
+                line = readLine(streamReader);
+                action = Integer.parseInt(line);
+                System.out.println();
+            } while (action < 1 || action > 3);
+
+            switch (action) {
+                case 1:
+                    showUser(login, password);
+                    break;
+                case 2:
+                    changeUserIO(login, password);
+                    System.out.println("Пользователь изменён");
+                    break;
+                case 3:
+                    deleteUser(login);
+                    System.out.println("Пользователь удалён");
+                    break;
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void showUser(String login, String password) {
+        StringBuilder sb = new StringBuilder();
+        try (FileReader fileReader = new FileReader("./users.txt")) {
+            String user = readLine(fileReader);
+            while (user != null) {
+                String[] userInfo = user.split(", ");
+                if (userInfo[0].equals(login) && userInfo[1].equals(password)) {
+                    sb.append("Name: ");
+                    sb.append(userInfo[2]);
+                    sb.append("\nIncome: ");
+                    sb.append(userInfo[3]);
+                    break;
+                }
+                user = readLine(fileReader);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(sb);
+    }
+
+    public static void changeUserIO(String login, String password) {
+        try (InputStreamReader streamReader = new InputStreamReader(System.in)) {
+            System.out.print("Введите новое Имя: ");
+            String name = readLine(streamReader);
+            System.out.print("Введите новый доход: ");
+            String income = readLine(streamReader);
+            System.out.println();
+            changeUser(login, password, name, income);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void changeUser(String login, String password, String name, String income) {
+        deleteUser(login);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(login);
+        sb.append(", ");
+        sb.append(password);
+        sb.append(", ");
+        sb.append(name);
+        sb.append(", ");
+        sb.append(income);
+
+        try (FileWriter fileWriter = new FileWriter("./users.txt", true)) {
+            fileWriter.write(sb.toString() + "\n");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void deleteUser(String login) {
+        getUsersListFromFile();
+        for (User user : userList) {
+            if (user.login.equals(login)) userList.remove(user);
+        }
+
+        /*try (FileWriter fileWriter = new FileWriter("./users_temp.txt", false)) {
+            fileWriter.write("");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }*/
+
+        /*try (FileReader fileReader = new FileReader("./users.txt");
+             FileWriter fileWriter = new FileWriter("./users_temp.txt", true)) {
+            String user = readLine(fileReader);
+            while (user != null) {
+                String[] userInfo = user.split(", ");
+                if (!userInfo[0].equals(login)) {
+                    fileWriter.write(user + "\n");
+                }
+                user = readLine(fileReader);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }*/
+
+        try (FileWriter fileWriter = new FileWriter("./users.txt", false)) {
+            for (User user : userList) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(user.login);
+                sb.append(", ");
+                sb.append(user.password);
+                sb.append(", ");
+                sb.append(user.name);
+                sb.append(", ");
+                sb.append(user.income);
+                fileWriter.write(sb.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void getUsersListFromFile() {
+        try (FileReader fileReader = new FileReader("./users.txt")) {
+            String user = readLine(fileReader);
+            while (user != null) {
+                String[] userInfo = user.split(", ");
+
+                User userOb = new User();
+                userOb.login = userInfo[0];
+                userOb.password = userInfo[1];
+                userOb.name = userInfo[2];
+                userOb.income = userInfo[3];
+                userList.add(userOb);
+
+                user = readLine(fileReader);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void getUsersFromFile() {
+        try (FileReader fileReader = new FileReader("./users.txt")) {
+            String user = readLine(fileReader);
+            while (user != null) {
+                String[] userInfo = user.split(", ");
+                users.put(userInfo[0], userInfo[1]);
+                user = readLine(fileReader);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
